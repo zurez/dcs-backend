769c43830691d4ebb2a5235973887193d89545b6
Md Zurez Tuba <zurez@users.noreply.github.com>
2020-07-03 15:52:19 +0530

    babelrc

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import ApiRoutes from "./routes/api.routes";
 import bodyParser from "body-parser";
+import helmet from "helmet";
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/", ApiRoutes);
